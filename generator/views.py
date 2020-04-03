e982f9d18c44a1886a2b79b4ebb3980422ffec00
Kashif1Naqvi <mrkashif029@gmail.com>
2020-04-04 00:44:21 +0500

    code updated

--- generator/views.py
+++ generator/views.py
@@ -4,12 +4,12 @@ import random
 
 def home(request):
   return render(request,'generator/home.html')
+
 def about(request):
   return render(request,'generator/about.html')
+
 def password(request):
-  character=list()
-  if request.GET.get('lowercase'):
-    character = list('abcdefghijklmnopqrstuvwxyz')
+  character = list('abcdefghijklmnopqrstuvwxyz')
   if request.GET.get('uppercase'):
       character.extend('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
   if request.GET.get('specialchar'):
