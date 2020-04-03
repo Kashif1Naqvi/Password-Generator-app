1bc379992fd928b99da52d435823bb6c253caeb5
Kashif1Naqvi <mrkashif029@gmail.com>
2020-04-03 15:35:54 +0500

    code updated

--- generator/views.py
+++ generator/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
-from django.auth import HttpRequestResponce
+import random
 # Create your views here.
 
 def home(request):
-  return HttpRequestResponce("Hello i am new i conding!")
+  return render(request,'generator/home.html')
+def about(request):
+  return render(request,'generator/about.html')
+def password(request):
+  character=list()
+  if request.GET.get('lowercase'):
+    character = list('abcdefghijklmnopqrstuvwxyz')
+  if request.GET.get('uppercase'):
+      character.extend('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+  if request.GET.get('specialchar'):
+      character.extend('~!@#$%^&*()_+}{|":>?<>/')
+  if request.GET.get('number'):
+      character.extend('0123456789')
+  length = int(request.GET.get('length'))
+  thepassword = ''
+  for i in range(length):
+      thepassword += random.choice(character)
+  return render(request,'generator/password.html',{'password':thepassword})
--- password_generator_project/urls.py
+++ password_generator_project/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path,include
+from generator import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('',views.home , name="home"),
+    path('password/',views.password, name="password"),
+    path('about/'   ,views.about   , name="about" ),
 ]
