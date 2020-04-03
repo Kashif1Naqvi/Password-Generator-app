a5044804fc3e330b3e7f4fc9722c4afdb6a33638
Kashif1Naqvi <mrkashif029@gmail.com>
2020-04-03 11:38:23 +0500

    views changed

--- generator/views.py
+++ generator/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.auth import HttpRequestResponce
 # Create your views here.
+
+def home(request):
+  return HttpRequestResponce("Hello i am new i conding!")
